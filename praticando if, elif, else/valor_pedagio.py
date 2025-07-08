332a0dd8085628dba17f599bcc8cdc22a24467f4
bezmari19 <marina.bezerraf19@gmail.com>
2025-07-08 17:02:42 -0300

    :books:docs: exercicio valor pedagio com comentários

--- praticando if, elif, else/valor_pedagio.py
+++ praticando if, elif, else/valor_pedagio.py
@@ -2,9 +2,9 @@
 
 distancia = int(input('Digite a distancia percorrida:\n'))
 
-if distancia < 100:
-    print(f'A distancia percorrida foi {distancia}. Valor: R$10,00')
-elif 100 <= distancia < 200:
-    print(f'A distancia percorrida foi {distancia}. Valor: R$20,00')
-elif distancia > 200:
-    print(f'A distancia percorrida foi: {distancia}. Valor: R$30,00')
+if distancia < 100: #se a distancia percorrida for menor que 100km
+    print(f'A distancia percorrida foi {distancia}. Valor: R$10,00') #exibe o valor correspondente e confirma o que foi digitado pelo usuário
+elif 100 <= distancia < 200: #se a distancia for entre 100 e 200
+    print(f'A distancia percorrida foi {distancia}. Valor: R$20,00') #exibe o valor correspondente e confirma o que foi digitado pelo usuário
+elif distancia > 200: #se a distancia percorrida for maior que 200
+    print(f'A distancia percorrida foi: {distancia}. Valor: R$30,00') #exibe o valor correspondente e confirma o que foi digitado pelo usuário
